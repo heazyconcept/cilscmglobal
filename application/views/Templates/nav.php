63f676536d45d2fb0c4b56b1416b063c9ab05d2f
Ezekiel Fadipe <ezekiel.fadipe@outlook.com>
2020-05-16 11:36:35 +0100

    final changes

--- application/views/Templates/nav.php
+++ application/views/Templates/nav.php
@@ -1,44 +1,51 @@
-<nav class="site-navbar navbar navbar-default navbar-fixed-top navbar-mega navbar-inverse"
-    role="navigation">
+<nav class="site-navbar navbar navbar-default navbar-fixed-top navbar-mega navbar-inverse" role="navigation">
 
     <div class="navbar-header">
-      <button type="button" class="navbar-toggler hamburger hamburger-close navbar-toggler-left hided"
-        data-toggle="menubar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="hamburger-bar"></span>
-      </button>
-      <button type="button" class="navbar-toggler collapsed" data-target="#site-navbar-collapse"
-        data-toggle="collapse">
-        <i class="icon wb-more-horizontal" aria-hidden="true"></i>
-      </button>
-      <a class="navbar-brand navbar-brand-center" href="<?php echo base_url(); ?>">
-        <img class="navbar-brand-logo navbar-brand-logo-normal" src="https://www.cilscmglobal.org/wp-content/uploads/2018/06/cilscm_logo.png"
-          title="cilscmglobal">
-        <img class="navbar-brand-logo navbar-brand-logo-special" src="https://www.cilscmglobal.org/wp-content/uploads/2018/06/cilscm_logo.png"
-          title="cilscmglobal">
-      </a>
-    
+        <button type="button" class="navbar-toggler hamburger hamburger-close navbar-toggler-left hided"
+            data-toggle="menubar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="hamburger-bar"></span>
+        </button>
+        <button type="button" class="navbar-toggler collapsed" data-target="#site-navbar-collapse"
+            data-toggle="collapse">
+            <i class="icon wb-more-horizontal" aria-hidden="true"></i>
+        </button>
+        <a class="navbar-brand navbar-brand-center" href="<?php echo base_url(); ?>">
+            <img class="navbar-brand-logo navbar-brand-logo-normal"
+                src="https://www.cilscmglobal.org/wp-content/uploads/2018/06/cilscm_logo.png" title="cilscmglobal">
+            <img class="navbar-brand-logo navbar-brand-logo-special"
+                src="https://www.cilscmglobal.org/wp-content/uploads/2018/06/cilscm_logo.png" title="cilscmglobal">
+        </a>
+
     </div>
 
     <div class="navbar-container container-fluid">
-      <!-- Navbar Collapse -->
-      <div class="collapse navbar-collapse navbar-collapse-toolbar" id="site-navbar-collapse">
-        <!-- Navbar Toolbar -->
-        <ul class="nav navbar-toolbar">
-          
-         
-         
-          
-        </ul>
-        <!-- End Navbar Toolbar -->
+        <!-- Navbar Collapse -->
+        <div class="collapse navbar-collapse navbar-collapse-toolbar" id="site-navbar-collapse">
+            <!-- Navbar Toolbar -->
+            <ul class="nav navbar-toolbar">
 
-        <!-- Navbar Toolbar Right -->
-        <ul class="nav navbar-toolbar navbar-right navbar-toolbar-right">
-          <li class="nav-item">
-            <a href="<?php echo base_url('account/login') ?>" class="nav-link btn btn-info">LOGIN</a>
-          </li>
-        
-          <!-- <li class="nav-item dropdown">
+
+
+
+            </ul>
+            <!-- End Navbar Toolbar -->
+            <?php $userId = $this->utilities->GetSessionId(); ?>
+            <!-- Navbar Toolbar Right -->
+            <ul class="nav navbar-toolbar navbar-right navbar-toolbar-right">
+                <?php if (empty($userId)): ?>
+                  <li class="nav-item">
+                      <a href="<?php echo base_url('account/login') ?>" class="nav-link btn btn-info">LOGIN</a>
+                  </li>
+
+                <?php else: ?>
+                  <li class="nav-item">
+                      <a href="<?php echo base_url('account/logout') ?>" class="nav-link btn btn-info btn-danger">LOGOUT</a>
+                  </li>
+                <?php endif; ?>
+
+
+                <!-- <li class="nav-item dropdown">
             <a class="nav-link navbar-avatar" data-toggle="dropdown" href="#" aria-expanded="false"
               data-animation="scale-up" role="button">
               <span class="avatar avatar-online">
@@ -55,16 +62,15 @@
             </div>
           </li>
          -->
-          
-         
-        </ul>
-        <!-- End Navbar Toolbar Right -->
-      </div>
-      <!-- End Navbar Collapse -->
 
-      <!-- Site Navbar Seach -->
-     
-      <!-- End Site Navbar Seach -->
+
+            </ul>
+            <!-- End Navbar Toolbar Right -->
+        </div>
+        <!-- End Navbar Collapse -->
+
+        <!-- Site Navbar Seach -->
+
+        <!-- End Site Navbar Seach -->
     </div>
-  </nav>
- 
+</nav>
